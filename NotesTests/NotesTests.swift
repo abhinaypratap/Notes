7d10090a4fafe4d97feb1622980ea7aa0e959ee0
Abhinay Pratap <abhinaypratap@icloud.com>
2023-03-28 22:30:02 +0530

    Create project.

---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import Notes
+
+final class NotesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+
+        }
+    }
+
+}
